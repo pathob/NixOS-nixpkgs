b5bd687034fc3040f3c4be5dde5ecdd3f80efe5b
Patrick Hobusch <patrick@hobusch.net>
2024-05-10 13:39:08 +0800

    python311Packages.tinytuya: init at 0.1.51

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15166,6 +15166,8 @@ self: super: with self; {
 
   tinysegmenter = callPackage ../development/python-modules/tinysegmenter { };
 
+  tinytuya = callPackage ../development/python-modules/tinytuya { };
+
   tissue = callPackage ../development/python-modules/tissue { };
 
   titlecase = callPackage ../development/python-modules/titlecase { };
