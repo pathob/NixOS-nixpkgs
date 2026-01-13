0a64fb658c7b9772b0a88f99891740a8ae291b21
Patrick Hobusch <patrick@hobusch.net>
2026-01-14 00:03:23 +0100

    WIP

--- pkgs/applications/editors/vscode/extensions/ms-vscode.vscode-speech/default.nix
+++ pkgs/applications/editors/vscode/extensions/ms-vscode.vscode-speech/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   vscode-utils,
+  vscode-extension-update-script,
   autoPatchelfHook,
   stdenv,
   alsa-lib,
@@ -15,9 +16,17 @@ vscode-utils.buildVscodeMarketplaceExtension {
           arch = "linux-x64";
           hash = "sha256-dZwOBehoYEqaYskvcPB55IKnG1CMToioyUJXlndqorA=";
         };
+        "x86_64-darwin" = {
+          arch = "darwin-x64";
+          hash = "sha256-b6LobvVngC0TFuWTC9JBQrECkoX7ewLNCpCROkXHk20=";
+        };
         "aarch64-linux" = {
           arch = "linux-arm64";
-          hash = "sha256-Dj+E1hPLaO2SCBvYqn3zfyJnNz30/V0xR2T8q8VNkXE=";
+          hash = "sha256-/EaOfoubfq1ufwB7TTQ2hqmh1ZJiZ1+B6QeYu3MoFPI=";
+        };
+        "aarch64-darwin" = {
+          arch = "darwin-arm64";
+          hash = "sha256-JhZWNlGXljsjmT3/xDi9Z7I4a2vsi/9EkWYbnlteE98=";
         };
       };
     in
@@ -29,35 +38,39 @@ vscode-utils.buildVscodeMarketplaceExtension {
     // sources.${stdenv.hostPlatform.system}
       or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
 
-  nativeBuildInputs = [
+  nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [
     autoPatchelfHook
   ];
 
-  buildInputs = [
+  buildInputs = lib.optionals stdenv.hostPlatform.isLinux [
     stdenv.cc.cc.lib
     alsa-lib
     libuuid
   ];
 
   # Prevent fixup phase from shrinking RPATHs - we need the Release directory
-  # in the RPATH for dlopen to find audio.sys.so at runtime
+  # in the RPATH for dlopen to find libMicrosoft.CognitiveServices.Speech.extension.audio.sys.so
   dontPatchELF = true;
 
-  # The core.so library uses dlopen to load audio.sys.so at runtime.
+  # libMicrosoft.CognitiveServices.Speech.core.so uses dlopen to load audio.sys.so at runtime.
   # autoPatchelfHook patches direct dependencies but can't detect dlopen calls,
   # so we add the Release directory to RPATH.
-  appendRunpaths = [
+  appendRunpaths = lib.optionals stdenv.hostPlatform.isLinux [
     "${placeholder "out"}/share/vscode/extensions/ms-vscode.vscode-speech/node_modules/@vscode/node-speech/build/Release"
   ];
+
+  passthru.updateScript = vscode-extension-update-script { };
 
   meta = {
     description = "Enables speech-to-text and text-to-speech capabilities in VS Code";
     downloadPage = "https://marketplace.visualstudio.com/items?itemName=ms-vscode.vscode-speech";
-    homepage = "https://github.com/microsoft/vscode-speech";
+    homepage = "https://github.com/microsoft/vscode";
     license = lib.licenses.unfree;
     platforms = [
       "x86_64-linux"
+      "x86_64-darwin"
       "aarch64-linux"
+      "aarch64-darwin"
     ];
     maintainers = [ ];
   };
