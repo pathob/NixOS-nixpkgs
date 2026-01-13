353e3ad1b5f1b3428496afad86e107dab02fc7ae
Patrick Hobusch <patrick@hobusch.net>
2026-01-13 23:37:53 +0100

    WIP

--- pkgs/applications/editors/vscode/extensions/ms-vscode.vscode-speech/default.nix
+++ pkgs/applications/editors/vscode/extensions/ms-vscode.vscode-speech/default.nix
@@ -2,7 +2,6 @@
   lib,
   vscode-utils,
   autoPatchelfHook,
-  patchelf,
   stdenv,
   alsa-lib,
   libuuid,
@@ -22,78 +21,37 @@ let
     };
   };
 
-  base =
-    supported.${system}
-      or (throw "Unsupported system: ${system}");
+  base = supported.${system} or (throw "Unsupported system: ${system}");
 
-  version = "0.16.0";
-
-  # First stage: build with autoPatchelfHook
-  unpacked = vscode-utils.buildVscodeMarketplaceExtension {
-    mktplcRef = base // {
-      name = "vscode-speech";
-      publisher = "ms-vscode";
-      inherit version;
-    };
-
-    nativeBuildInputs = [
-      autoPatchelfHook
-    ];
-
-    buildInputs = [
-      stdenv.cc.cc.lib
-      alsa-lib
-      libuuid
-    ];
+  releaseDir = "share/vscode/extensions/ms-vscode.vscode-speech/node_modules/@vscode/node-speech/build/Release";
+in
+vscode-utils.buildVscodeMarketplaceExtension {
+  mktplcRef = base // {
+    name = "vscode-speech";
+    publisher = "ms-vscode";
+    version = "0.16.0";
   };
 
-  # Second stage: fix dlopen RPATH for audio.sys.so
-  # The core.so library uses dlopen to load audio.sys.so at runtime.
-  # autoPatchelfHook patches the direct dependencies but can't detect dlopen calls.
-  # We copy the extension and re-patch ALL native libraries to:
-  # 1. Update RPATHs from the unpacked store path to our new $out path
-  # 2. Add the Release directory to core.so's RPATH for dlopen
-in
-stdenv.mkDerivation {
-  pname = "vscode-extension-ms-vscode-vscode-speech";
-  inherit version;
+  nativeBuildInputs = [
+    autoPatchelfHook
+  ];
 
-  dontUnpack = true;
+  buildInputs = [
+    stdenv.cc.cc.lib
+    alsa-lib
+    libuuid
+  ];
 
   # Prevent fixup phase from shrinking RPATHs - we need the Release directory
   # in the RPATH for dlopen to find audio.sys.so at runtime
   dontPatchELF = true;
 
-  nativeBuildInputs = [ patchelf ];
-
-  # Pass through attributes that home-manager/vscode-utils expects
-  inherit (unpacked) vscodeExtUniqueId vscodeExtPublisher vscodeExtName;
-
-  installPhase = ''
-    runHook preInstall
-
-    cp -r ${unpacked} $out
-    chmod -R u+w $out
-
-    releaseDir="$out/share/vscode/extensions/ms-vscode.vscode-speech/node_modules/@vscode/node-speech/build/Release"
-
-    # Update all native libraries to use the new store path instead of the old one
-    for file in "$releaseDir"/*.so "$releaseDir"/*.node; do
-      if [ -f "$file" ]; then
-        oldRpath=$(patchelf --print-rpath "$file" 2>/dev/null || true)
-        if [ -n "$oldRpath" ]; then
-          # Replace old store path with new one in RPATH
-          newRpath=$(echo "$oldRpath" | sed "s|${unpacked}|$out|g")
-          patchelf --set-rpath "$newRpath" "$file"
-        fi
-      fi
-    done
-
-    # Add the Release directory to core.so's RPATH for dlopen of audio.sys.so
-    patchelf --add-rpath "$releaseDir" "$releaseDir/libMicrosoft.CognitiveServices.Speech.core.so"
-
-    runHook postInstall
-  '';
+  # The core.so library uses dlopen to load audio.sys.so at runtime.
+  # autoPatchelfHook patches direct dependencies but can't detect dlopen calls,
+  # so we add the Release directory to RPATH.
+  appendRunpaths = [
+    "${placeholder "out"}/${releaseDir}"
+  ];
 
   meta = {
     description = "Enables speech-to-text and text-to-speech capabilities in VS Code";
